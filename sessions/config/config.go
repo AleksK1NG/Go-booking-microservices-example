a83fe28fe807dbed8b53d352c0365580563ad986
Alexander Bryksin <alexander.bryksin@yandex.ru>
2021-01-25 16:00:51 +0300

    add sessions microservice config

---
+++ sessions/config/config.go
@@ -0,0 +1,123 @@
+package config
+
+import (
+	"errors"
+	"log"
+
+	"github.com/spf13/viper"
+)
+
+// App config
+type Config struct {
+	GRPCServer GRPCServer
+	Postgres   PostgresConfig
+	Redis      RedisConfig
+	Metrics    Metrics
+	Logger     Logger
+	Jaeger     Jaeger
+}
+
+// GRPCServer config
+type GRPCServer struct {
+	Port           string
+	CookieLifeTime int
+	CsrfExpire     int
+	SessionID      string
+}
+
+// Logger config
+type Logger struct {
+	Development       bool
+	DisableCaller     bool
+	DisableStacktrace bool
+	Encoding          string
+	Level             string
+}
+
+// Postgresql config
+type PostgresConfig struct {
+	PostgresqlHost     string
+	PostgresqlPort     string
+	PostgresqlUser     string
+	PostgresqlPassword string
+	PostgresqlDbname   string
+	PostgresqlSSLMode  bool
+	PgDriver           string
+}
+
+// Redis config
+type RedisConfig struct {
+	RedisAddr      string
+	RedisPassword  string
+	RedisDB        string
+	RedisDefaultDB string
+	MinIdleConn    int
+	PoolSize       int
+	PoolTimeout    int
+	Password       string
+	DB             int
+}
+
+// Metrics config
+type Metrics struct {
+	URL         string
+	ServiceName string
+}
+
+// Jaeger
+type Jaeger struct {
+	Host        string
+	ServiceName string
+	LogSpans    bool
+}
+
+// Load config file from given path
+func LoadConfig(filename string) (*viper.Viper, error) {
+	v := viper.New()
+
+	v.SetConfigName(filename)
+	v.AddConfigPath(".")
+	v.AutomaticEnv()
+	if err := v.ReadInConfig(); err != nil {
+		if _, ok := err.(viper.ConfigFileNotFoundError); ok {
+			return nil, errors.New("config file not found")
+		}
+		return nil, err
+	}
+
+	return v, nil
+}
+
+// Parse config file
+func ParseConfig(v *viper.Viper) (*Config, error) {
+	var c Config
+
+	err := v.Unmarshal(&c)
+	if err != nil {
+		log.Printf("unable to decode into struct, %v", err)
+		return nil, err
+	}
+
+	return &c, nil
+}
+
+// Get config
+func GetConfig(configPath string) (*Config, error) {
+	cfgFile, err := LoadConfig(configPath)
+	if err != nil {
+		return nil, err
+	}
+
+	cfg, err := ParseConfig(cfgFile)
+	if err != nil {
+		return nil, err
+	}
+	return cfg, nil
+}
+
+func GetConfigPath(configPath string) string {
+	if configPath == "docker" {
+		return "./config/config-docker"
+	}
+	return "./config/config-local"
+}
