0e96a520c657f5e44f5574015560e53922dd8649
Alexander Bryksin <alexander.bryksin@yandex.ru>
2021-01-25 16:00:51 +0300

    add sessions microservice config

--- sessions/cmd/main.go
+++ sessions/cmd/main.go
@@ -1,7 +1,21 @@
 package main
 
-import "log"
+import (
+	"log"
+	"os"
+
+	"github.com/AleksK1NG/hotels-mocroservices/sessions/config"
+)
 
 func main() {
 	log.Println("Starting sessions server")
+
+	configPath := config.GetConfigPath(os.Getenv("config"))
+	cfg, err := config.GetConfig(configPath)
+	if err != nil {
+		log.Fatalf("Loading config: %v", err)
+	}
+
+	log.Printf("%-v", cfg)
+
 }
