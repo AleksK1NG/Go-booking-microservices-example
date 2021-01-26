821b12e42aa9c5896959aa0f98b0fdc7cf99b735
Alexander Bryksin <alexander.bryksin@yandex.ru>
2021-01-26 15:57:04 +0300

    add user microservice config init

--- user/cmd/main.go
+++ user/cmd/main.go
@@ -1,7 +1,20 @@
 package main
 
-import "log"
+import (
+	"log"
+	"os"
+
+	"github.com/AleksK1NG/hotels-mocroservices/user/config"
+)
 
 func main() {
 	log.Println("Starting user server")
+
+	configPath := config.GetConfigPath(os.Getenv("config"))
+	cfg, err := config.GetConfig(configPath)
+	if err != nil {
+		log.Fatalf("Loading config: %v", err)
+	}
+
+	log.Printf("%-v", cfg)
 }
