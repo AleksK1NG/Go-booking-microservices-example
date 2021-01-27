ade6894811700bbba04216599b9778e5a3ba3eb4
Alexander Bryksin <alexander.bryksin@yandex.ru>
2021-01-27 14:46:28 +0300

    add grpc client GetInterceptor

--- user/internal/session/grpc_client/grpc_client.go
+++ user/internal/session/grpc_client/grpc_client.go
@@ -6,11 +6,14 @@ import (
 	"google.golang.org/grpc"
 
 	"github.com/AleksK1NG/hotels-mocroservices/user/config"
+	"github.com/AleksK1NG/hotels-mocroservices/user/internal/interceptors"
+	"github.com/AleksK1NG/hotels-mocroservices/user/pkg/logger"
 )
 
 // NewSessionServiceClient
-func NewSessionServiceConn(ctx context.Context, cfg *config.Config) (*grpc.ClientConn, error) {
+func NewSessionServiceConn(ctx context.Context, cfg *config.Config, logger logger.Logger) (*grpc.ClientConn, error) {
 	sessGRPCConn, err := grpc.DialContext(ctx, cfg.GRPCServer.SessionGrpcServicePort,
+		grpc.WithUnaryInterceptor(interceptors.GetInterceptor(logger)),
 		grpc.WithInsecure(),
 	)
 	if err != nil {
