7bd133e86e338f6d9ec3ce68480ba13fda844119
Alexander Bryksin <alexander.bryksin@yandex.ru>
2021-01-31 14:25:44 +0300

    add rabbitmq image usecase publisher

--- images_microservice/internal/image/usecase/usecase.go
+++ images_microservice/internal/image/usecase/usecase.go
@@ -2,22 +2,26 @@ package usecase
 
 import (
 	"context"
+	"time"
 
 	"github.com/opentracing/opentracing-go"
+	"github.com/pkg/errors"
 	"github.com/streadway/amqp"
 
 	"github.com/AleksK1NG/hotels-mocroservices/images-microservice/internal/image"
+	"github.com/AleksK1NG/hotels-mocroservices/images-microservice/internal/image/publisher"
 	"github.com/AleksK1NG/hotels-mocroservices/images-microservice/pkg/logger"
 )
 
 type ImageUseCase struct {
-	pgRepo  image.PgRepository
-	awsRepo image.AWSRepository
-	logger  logger.Logger
+	pgRepo    image.PgRepository
+	awsRepo   image.AWSRepository
+	logger    logger.Logger
+	publisher publisher.Publisher
 }
 
-func NewImageUseCase(pgRepo image.PgRepository, awsRepo image.AWSRepository, logger logger.Logger) *ImageUseCase {
-	return &ImageUseCase{pgRepo: pgRepo, awsRepo: awsRepo, logger: logger}
+func NewImageUseCase(pgRepo image.PgRepository, awsRepo image.AWSRepository, logger logger.Logger, publisher publisher.Publisher) *ImageUseCase {
+	return &ImageUseCase{pgRepo: pgRepo, awsRepo: awsRepo, logger: logger, publisher: publisher}
 }
 
 func (i *ImageUseCase) ResizeImage(ctx context.Context, delivery amqp.Delivery) error {
@@ -25,6 +29,10 @@ func (i *ImageUseCase) ResizeImage(ctx context.Context, delivery amqp.Delivery) 
 	defer span.Finish()
 
 	i.logger.Infof("amqp.Delivery: %-v", delivery)
+
+	if err := i.publisher.Publish("images", "uploaded", "image/jpeg", []byte(time.Now().String())); err != nil {
+		return errors.Wrap(err, "ImageUseCase.ResizeImage.Publish")
+	}
 
 	return nil
 }
