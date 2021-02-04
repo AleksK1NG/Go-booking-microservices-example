cfb4c6d97fff2d14029afd9374c083c563f8e1e0
Alexander Bryksin <alexander.bryksin@yandex.ru>
2021-02-04 17:04:48 +0300

    add images grpc get by id method

--- images_microservice/internal/image/delivery/grpc/grpc_image.go
+++ images_microservice/internal/image/delivery/grpc/grpc_image.go
@@ -3,20 +3,42 @@ package grpc
 import (
 	"context"
 
+	"github.com/opentracing/opentracing-go"
+	uuid "github.com/satori/go.uuid"
+	"google.golang.org/grpc/status"
+
 	"github.com/AleksK1NG/hotels-mocroservices/images-microservice/config"
+	"github.com/AleksK1NG/hotels-mocroservices/images-microservice/internal/image"
+	"github.com/AleksK1NG/hotels-mocroservices/images-microservice/pkg/grpc_errors"
 	"github.com/AleksK1NG/hotels-mocroservices/images-microservice/pkg/logger"
 	"github.com/AleksK1NG/hotels-mocroservices/images-microservice/proto/image"
 )
 
 type ImageService struct {
-	cfg    *config.Config
-	logger logger.Logger
+	cfg     *config.Config
+	logger  logger.Logger
+	imageUC image.UseCase
 }
 
-func NewImageService(cfg *config.Config, logger logger.Logger) *ImageService {
-	return &ImageService{cfg: cfg, logger: logger}
+func NewImageService(cfg *config.Config, logger logger.Logger, imageUC image.UseCase) *ImageService {
+	return &ImageService{cfg: cfg, logger: logger, imageUC: imageUC}
 }
 
 func (i *ImageService) GetImageByID(ctx context.Context, req *imageService.GetByIDRequest) (*imageService.GetByIDResponse, error) {
-	panic("implement me")
+	span, ctx := opentracing.StartSpanFromContext(ctx, "ImageService.GetImageByID")
+	defer span.Finish()
+
+	imageUUID, err := uuid.FromString(req.GetImageID())
+	if err != nil {
+		i.logger.Errorf("uuid.FromString: %v", err)
+		return nil, status.Errorf(grpc_errors.ParseGRPCErrStatusCode(err), "uuid.FromString: %v", err)
+	}
+
+	imageByID, err := i.imageUC.GetImageByID(ctx, imageUUID)
+	if err != nil {
+		i.logger.Errorf("uuid.FromString: %v", err)
+		return nil, status.Errorf(grpc_errors.ParseGRPCErrStatusCode(err), "mageUC.GetImageByID: %v", err)
+	}
+
+	return &imageService.GetByIDResponse{Image: imageByID.ToProto()}, nil
 }
--- images_microservice/internal/image/pg_repository.go
+++ images_microservice/internal/image/pg_repository.go
@@ -3,9 +3,12 @@ package image
 import (
 	"context"
 
+	uuid "github.com/satori/go.uuid"
+
 	"github.com/AleksK1NG/hotels-mocroservices/images-microservice/internal/models"
 )
 
 type PgRepository interface {
 	Create(ctx context.Context, msg *models.Image) (*models.Image, error)
+	GetImageByID(ctx context.Context, imageID uuid.UUID) (*models.Image, error)
 }
