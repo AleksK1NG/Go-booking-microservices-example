d058efee31b2a8b08154cce1507d376c19fd5d5d
Alexander Bryksin <alexander.bryksin@yandex.ru>
2021-02-05 14:42:59 +0300

    add images service delete unused code

--- images_microservice/internal/image/repository/sql_queries.go
+++ images_microservice/internal/image/repository/sql_queries.go
@@ -4,4 +4,6 @@ const (
 	createImageQuery = `INSERT INTO images (image_url, is_uploaded) 
 	VALUES ($1, $2) 
 	RETURNING image_id, image_url, is_uploaded, created_at`
+
+	getImageByIDQuery = `SELECT image_id, image_url, is_uploaded, created_at, updated_at FROM images WHERE image_id = $1`
 )
