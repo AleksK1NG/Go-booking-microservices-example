ac58287820bde79e01e61dc77b626328684ad0a7
Alexander Bryksin <alexander.bryksin@yandex.ru>
2021-02-07 13:31:06 +0300

    add hotels update method

--- hotels/internal/hotels/repository/pg_repository.go
+++ hotels/internal/hotels/repository/pg_repository.go
@@ -25,7 +25,7 @@ func (h *HotelsPGRepository) CreateHotel(ctx context.Context, hotel *models.Hote
 	span, ctx := opentracing.StartSpanFromContext(ctx, "HotelsPGRepository.CreateHotel")
 	defer span.Finish()
 
-	point := utils.GeneratePointToGeoFromFloat64(hotel.Latitude.Float64, hotel.Longitude.Float64)
+	point := utils.GeneratePointToGeoFromFloat64(*hotel.Latitude, *hotel.Longitude)
 	createHotelQuery := `INSERT INTO hotels (name, location, description, image, photos, coordinates, email, country, city, rating) 
 	VALUES ($1, $2, $3, $4, $5, ST_GeomFromEWKT($6), $7, $8, $9, $10) RETURNING hotel_id, created_at, updated_at`
 
@@ -36,7 +36,7 @@ func (h *HotelsPGRepository) CreateHotel(ctx context.Context, hotel *models.Hote
 		hotel.Name,
 		hotel.Location,
 		hotel.Description,
-		hotel.Image.String,
+		hotel.Image,
 		hotel.Photos,
 		point,
 		hotel.Email,
@@ -56,10 +56,56 @@ func (h *HotelsPGRepository) CreateHotel(ctx context.Context, hotel *models.Hote
 	return hotel, nil
 }
 
+// UpdateHotel update single hotel
 func (h *HotelsPGRepository) UpdateHotel(ctx context.Context, hotel *models.Hotel) (*models.Hotel, error) {
-	panic("implement me")
+	span, ctx := opentracing.StartSpanFromContext(ctx, "HotelsPGRepository.UpdateHotel")
+	defer span.Finish()
+
+	updateHotelQuery := `UPDATE hotels 
+		SET email = COALESCE(NULLIF('', $1), email), name = $2, location = $3, description = $4, 
+	 	country = $5, city = $6, coordinates = ST_GeomFromEWKT($7)
+		WHERE hotel_id = $8
+	    RETURNING hotel_id, email, name, location, description, comments_count, 
+       	country, city, ((coordinates::POINT)[0])::decimal, ((coordinates::POINT)[1])::decimal, rating, photos, image, created_at, updated_at`
+
+	point := utils.GeneratePointToGeoFromFloat64(*hotel.Latitude, *hotel.Longitude)
+	var res models.Hotel
+	if err := h.db.QueryRow(
+		ctx,
+		updateHotelQuery,
+		hotel.Email,
+		hotel.Name,
+		hotel.Location,
+		hotel.Description,
+		hotel.Country,
+		hotel.City,
+		point,
+		hotel.HotelID,
+	).Scan(&hotel.HotelID,
+		&res.Email,
+		&res.Name,
+		&res.Location,
+		&res.Description,
+		&res.CommentsCount,
+		&res.Country,
+		&res.City,
+		&res.Latitude,
+		&res.Longitude,
+		&res.Rating,
+		&res.Photos,
+		&res.Image,
+		&res.CreatedAt,
+		&res.UpdatedAt,
+	); err != nil {
+		return nil, errors.Wrap(err, "db.QueryRow.Scan")
+	}
+
+	log.Printf("HOTEL BY ID: %v", res)
+
+	return &res, nil
 }
 
+// GetHotelByID get hotel by id
 func (h *HotelsPGRepository) GetHotelByID(ctx context.Context, hotelID uuid.UUID) (*models.Hotel, error) {
 	span, ctx := opentracing.StartSpanFromContext(ctx, "HotelsPGRepository.GetHotelByID")
 	defer span.Finish()
