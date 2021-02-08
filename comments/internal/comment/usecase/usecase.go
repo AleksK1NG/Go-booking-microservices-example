c181896fc3300e93f77d24b82fe5457fa712518a
Alexander Bryksin <alexander.bryksin@yandex.ru>
2021-02-08 17:19:33 +0300

    add comments gRPC service create comment method

--- comments/internal/comment/usecase/usecase.go
+++ comments/internal/comment/usecase/usecase.go
@@ -3,6 +3,8 @@ package usecase
 import (
 	"context"
 
+	"github.com/opentracing/opentracing-go"
+
 	"github.com/AleksK1NG/hotels-mocroservices/comments/internal/comment"
 	"github.com/AleksK1NG/hotels-mocroservices/comments/internal/models"
 	"github.com/AleksK1NG/hotels-mocroservices/comments/pkg/logger"
@@ -21,5 +23,7 @@ func NewCommUseCase(commRepo comment.PGRepository, logger logger.Logger) *commUs
 
 // Create
 func (c *commUseCase) Create(ctx context.Context, comment *models.Comment) (*models.Comment, error) {
-	panic("implement me")
+	span, ctx := opentracing.StartSpanFromContext(ctx, "commUseCase.Create")
+	defer span.Finish()
+	return c.commRepo.Create(ctx, comment)
 }
