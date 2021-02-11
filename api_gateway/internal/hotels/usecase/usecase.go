a9735810d0864b51c2c77e27cfafd92262345527
Alexander Bryksin <alexander.bryksin@yandex.ru>
2021-02-11 18:35:15 +0300

    add api gateway hotels domain usecase

--- api_gateway/internal/hotels/usecase/usecase.go
+++ api_gateway/internal/hotels/usecase/usecase.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/opentracing/opentracing-go"
 	"github.com/pkg/errors"
+	uuid "github.com/satori/go.uuid"
 
 	"github.com/AleksK1NG/hotels-mocroservices/api-gateway/internal/hotels"
 	"github.com/AleksK1NG/hotels-mocroservices/api-gateway/internal/models"
@@ -24,6 +25,104 @@ func NewHotelsUseCase(logger logger.Logger, hotelsService hotelsService.HotelsSe
 	return &hotelsUseCase{logger: logger, hotelsService: hotelsService, hotelsRepo: hotelsRepo}
 }
 
+// GetHotelByID
+func (h *hotelsUseCase) GetHotelByID(ctx context.Context, hotelID uuid.UUID) (*models.Hotel, error) {
+	span, ctx := opentracing.StartSpanFromContext(ctx, "HotelsUseCase.GetHotelByID")
+	defer span.Finish()
+
+	hotelByID, err := h.hotelsService.GetHotelByID(ctx, &hotelsService.GetByIDReq{HotelID: hotelID.String()})
+	if err != nil {
+		return nil, errors.Wrap(err, "hotelsService.GetHotelByID")
+	}
+
+	fromProto, err := models.HotelFromProto(hotelByID.GetHotel())
+	if err != nil {
+		return nil, errors.Wrap(err, "HotelFromProto")
+	}
+
+	return fromProto, nil
+}
+
+// UpdateHotel
+func (h *hotelsUseCase) UpdateHotel(ctx context.Context, hotel *models.Hotel) (*models.Hotel, error) {
+	span, ctx := opentracing.StartSpanFromContext(ctx, "HotelsUseCase.UpdateHotel")
+	defer span.Finish()
+
+	hotelRes, err := h.hotelsService.UpdateHotel(ctx, &hotelsService.UpdateHotelReq{
+		HotelID:       hotel.HotelID.String(),
+		Name:          hotel.Name,
+		Email:         hotel.Email,
+		Country:       hotel.Country,
+		City:          hotel.City,
+		Description:   hotel.Description,
+		Location:      hotel.Location,
+		Rating:        hotel.Rating,
+		Image:         *hotel.Image,
+		Photos:        hotel.Photos,
+		CommentsCount: int64(hotel.CommentsCount),
+		Latitude:      *hotel.Latitude,
+		Longitude:     *hotel.Longitude,
+	})
+	if err != nil {
+		return nil, errors.Wrap(err, "hotelsService.UpdateHotel")
+	}
+
+	fromProto, err := models.HotelFromProto(hotelRes.GetHotel())
+	if err != nil {
+		return nil, errors.Wrap(err, "HotelFromProto")
+	}
+
+	return fromProto, nil
+}
+
+// CreateHotel
+func (h *hotelsUseCase) CreateHotel(ctx context.Context, hotel *models.Hotel) (*models.Hotel, error) {
+	span, ctx := opentracing.StartSpanFromContext(ctx, "HotelsUseCase.CreateHotel")
+	defer span.Finish()
+
+	hotelRes, err := h.hotelsService.CreateHotel(ctx, &hotelsService.CreateHotelReq{
+		Name:          hotel.Name,
+		Email:         hotel.Email,
+		Country:       hotel.Country,
+		City:          hotel.City,
+		Description:   hotel.Description,
+		Location:      hotel.Location,
+		Rating:        hotel.Rating,
+		Image:         *hotel.Image,
+		Photos:        hotel.Photos,
+		CommentsCount: int64(hotel.CommentsCount),
+		Latitude:      *hotel.Latitude,
+		Longitude:     *hotel.Longitude,
+	})
+	if err != nil {
+		return nil, errors.Wrap(err, "hotelsService.CreateHotel")
+	}
+
+	fromProto, err := models.HotelFromProto(hotelRes.GetHotel())
+	if err != nil {
+		return nil, errors.Wrap(err, "HotelFromProto")
+	}
+
+	return fromProto, nil
+}
+
+// UploadImage
+func (h *hotelsUseCase) UploadImage(ctx context.Context, data []byte, contentType, hotelID string) error {
+	span, ctx := opentracing.StartSpanFromContext(ctx, "HotelsUseCase.UploadImage")
+	defer span.Finish()
+
+	_, err := h.hotelsService.UploadImage(ctx, &hotelsService.UploadImageReq{
+		HotelID:     hotelID,
+		Data:        data,
+		ContentType: contentType,
+	})
+	if err != nil {
+		return errors.Wrap(err, "hotelsService.UploadImage")
+	}
+
+	return nil
+}
+
 // GetHotelsGetHotels
 func (h *hotelsUseCase) GetHotels(ctx context.Context, page, size int64) (*models.HotelsListRes, error) {
 	span, ctx := opentracing.StartSpanFromContext(ctx, "HotelsUseCase.GetHotels")
@@ -41,7 +140,7 @@ func (h *hotelsUseCase) GetHotels(ctx context.Context, page, size int64) (*model
 	for _, v := range hotelsRes.Hotels {
 		hotel, err := models.HotelFromProto(v)
 		if err != nil {
-			return nil, err
+			return nil, errors.Wrap(err, "HotelFromProto")
 		}
 		hotelsList = append(hotelsList, hotel)
 	}
