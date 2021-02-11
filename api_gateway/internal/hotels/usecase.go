b43ebff69f3aeb22bc64b4456df2271bbc80d725
Alexander Bryksin <alexander.bryksin@yandex.ru>
2021-02-11 18:35:15 +0300

    add api gateway hotels domain usecase

--- api_gateway/internal/hotels/usecase.go
+++ api_gateway/internal/hotels/usecase.go
@@ -3,10 +3,16 @@ package hotels
 import (
 	"context"
 
+	uuid "github.com/satori/go.uuid"
+
 	"github.com/AleksK1NG/hotels-mocroservices/api-gateway/internal/models"
 )
 
 // UseCase
 type UseCase interface {
 	GetHotels(ctx context.Context, page, size int64) (*models.HotelsListRes, error)
+	GetHotelByID(ctx context.Context, hotelID uuid.UUID) (*models.Hotel, error)
+	UpdateHotel(ctx context.Context, hotel *models.Hotel) (*models.Hotel, error)
+	CreateHotel(ctx context.Context, hotel *models.Hotel) (*models.Hotel, error)
+	UploadImage(ctx context.Context, data []byte, contentType, hotelID string) error
 }
